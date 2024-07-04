17587563b40e1a53144d4c2c3be0c1f99254d726
HyperOTK <82686757+HyperOTK@users.noreply.github.com>
2024-07-04 01:58:57 -0600

    2nd Push

---
+++ Prueba_2/DirAlumnos/Alumno.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirAlumnos
+{
+    public class Usuario
+    {
+
+        public int userId { get; set; }
+
+        public string Login { get; set; }
+        public string Nombre { get; set; }
+        public string Paterno { get; set; }
+        public string Materno { get; set; }
+        public Double Sueldo { get; set; }
+        public String Ingreso { get; set; }
+
+        public Usuario() { }
+
+        public Usuario(int userId, string Login, string Nombre, string Paterno, string Materno, Double Sueldo, String Ingreso) 
+        { 
+        
+            this.userId = userId;  
+            this.Login= Login;
+            this.Nombre = Nombre; 
+            this.Paterno = Paterno;
+            this.Materno = Materno;
+            this.Sueldo = Sueldo;
+            this.Ingreso = Ingreso;
+
+
+        }
+    }
+}
---
+++ Prueba_2/DirAlumnos/Conexion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirAlumnos
+{
+    public class Conexion
+    {
+
+        public static SqlConnection EstableceConexion()
+        {
+            SqlConnection Conect = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=PRACTICASQL;Data Source=CYGNUSX-1\\SQLEXPRESS");
+            Conect.Open();
+
+            return Conect;
+
+        }
+    }
+}
